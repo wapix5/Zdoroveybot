b2ed5cd0df717d08bd98a1ce90218adb1690d514
wapix5 <werroma9@gmail.com>
2026-05-04 21:11:27 +0700

    seed.js

--- server/seed.js
+++ server/seed.js
@@ -1,9 +1,15 @@
-const db = require('./db');
+import { db } from "./db.js";
+
 db.exec("DELETE FROM order_items; DELETE FROM orders; DELETE FROM products;");
-const insert = db.prepare("INSERT INTO products (category,name,price,qty,image) VALUES (?,?,?,?,?)");
+
+const insert = db.prepare(
+  "INSERT INTO products (category,name,price,qty,image) VALUES (?,?,?,?,?)"
+);
+
 [
-  ["Спортпит","Креатин Monohydrate",800,10,"assets/demo.jpg"],
-  ["Витамины","Vitamin D3",550,6,"assets/demo.jpg"],
-  ["БАД","Ashwagandha",1500,4,"assets/demo.jpg"]
-].forEach(p => insert.run(...p));
+  ["Спортпит", "Креатин Monohydrate", 800, 10, "assets/demo.jpg"],
+  ["Витамины", "Vitamin D3", 550, 6, "assets/demo.jpg"],
+  ["БАД", "Ashwagandha", 1500, 4, "assets/demo.jpg"]
+].forEach(p => insert.run(p[0], p[1], p[2], p[3], p[4]));
+
 console.log("База заполнена тестовыми товарами.");
